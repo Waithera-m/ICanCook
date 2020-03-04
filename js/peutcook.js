ab6a95623cab8b1c0933dabb8699ccc8ac2683a1
raphkm <rkariuki415@gmail.com>
2020-03-04 10:53:33 +0300

    added sign in form

---
+++ js/peutcook.js
@@ -0,0 +1,86 @@
+// submit button function
+function validateTheForm() {
+    var Email= document.getElementById("Email").value;
+    var Password = document.getElementById("Password").value;
+    var Name = document.getElementById("Name").value;
+    if (Email == "") {
+      alert("please fill in your Email");
+      return false;
+    } 
+    else if (Password == "") {
+      alert("Please fill in your Password");
+      return false;
+    }
+     else {
+      alert(
+        Name+", we have received your message. Thank you for reaching out to us."
+      );
+    }
+    event.preventDefault();
+  }
+  function validateTheForm2() {
+    var Email= document.getElementById("Emailo").value;
+    var Password = document.getElementById("Passwordo").value;
+    if (Email == "") {
+      alert("please fill in your Email");
+      return false;
+    } 
+    else if (Password == "") {
+      alert("Please fill in your Password");
+      return false;
+    }
+     else {
+      alert(
+        "Hello, we have received your message. Thank you for reaching out to us."
+      );
+    }
+    event.preventDefault();
+  }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
