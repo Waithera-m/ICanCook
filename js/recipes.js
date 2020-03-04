4b6cd120656b19acbc8eb74397d382a1770c6ab0
Mary Waithera <njihiamary11@gmail.com>
2020-03-04 21:08:34 +0300

    Define variables to store users' input

--- js/recipes.js
+++ js/recipes.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var meals={
+  meals={
     combinedMeals:{
       "ricespinach":"https://www.allrecipes.com/recipe/72500/spinach-rice/",
       "ugaliSukumaMeat": "https://www.tasteoftheplace.com/a-kenyan-meal/",
@@ -23,5 +23,33 @@ $(document).ready(function(){
       "ugaliFishSukuma": "https://cookpad.com/ke/recipes/3416696-fried-fish-kales-and-kachumbari-with-ugali",
       "jollofRiceFish": "https://www.sbs.com.au/food/recipes/jollof-rice-fish"
     }
-  }
+  };
+
+  var allPurposeFlour = document.getElementById("allPurposeWheatFlour").value;
+  var maizeFlour = document.getElementById("mflour").value;
+  var pasta = document.getElementById("pasta").value;
+  var beans = document.getElementById("beans").value;
+  var lentils = document.getElementById("lentils").value;
+  var blackBeans = document.getElementById("blackbeans").value;
+  var milk = document.getElementById("milk").value;
+  var eggs = document.getElementById("eggs").value;
+  var cheddar = document.getElementById("cheddar").value;
+  var parmesan = document.getElementById("parmesan").value;
+  var yogurt = document.getElementById("yogurt").value;
+  var coconutMilk = document.getElementById("coconutMilk").value;
+  var beef = document.getElementById("beef").value;
+  var chicken = document.getElementById("chicken").value;
+  var bacon = document.getElementById("bacon").value;
+  var fish = document.getElementById("fish").value;
+  var pork = document.getElementById("pork").value;
+  var tomatoes = document.getElementById("tomatoes").value;
+  var cabbage = document.getElementById("cabbage").value;
+  var brocolli = document.getElementById("brocolli").value;
+  var cauliflower = document.getElementById("cauliflower").value;
+  var spinach = document.getElementById("spinach").value;
+  var sukuma = document.getElementById("sukuma").value;
+  var onion = document.getElementById("onion").value;
+  var garlic = document.getElementById("garlic").value;
+  var tangawizi = document.getElementById("tangawizi").value;
+
 });
