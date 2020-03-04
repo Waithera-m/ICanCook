4c84502b3a89242d054dada8afb6986244855c73
Mary Waithera <njihiamary11@gmail.com>
2020-03-04 13:58:40 +0300

    Add combined meals object and its respective key:value pairs

---
+++ js/recipes.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var meals={
+    combinedMeals:{
+      "ricespinach":"https://www.allrecipes.com/recipe/72500/spinach-rice/",
+      "ugaliSukumaMeat": "https://www.tasteoftheplace.com/a-kenyan-meal/",
+      "riceBeefStew": "https://shewearsmanyhats.com/easy-stew-beef-rice-recipe/",
+      "lasagna": "https://www.tasteofhome.com/recipes/best-lasagna/",
+      "parmesanwedges": "https://www.lecremedelacrumb.com/baked-garlic-parmesan-potato-wedges/",
+      "cheeseBacon": "https://www.bbcgoodfood.com/recipes/cheese-bacon-scones",
+      "baconOmelete": "https://www.recipetips.com/recipe-cards/t--37341/bacon-and-cheese-omelette.asp",
+      "riceFish": "https://cooking.nytimes.com/recipes/1016024-fish-stew-with-rice",
+      "beefTumbukiza": "https://cookpad.com/ke/recipes/5613019-beef-tumbukiza",
+      "porkUgali": "https://cookpad.com/ke/recipes/2511543-fried-pork-with-ugali",
+      "riceChicken": "https://shewearsmanyhats.com/chicken-and-rice-is-oh-so-nice/",
+      "ugaliChicken": "https://cookpad.com/ke/recipes/7415037-fried-chicken-with-ugali",
+      "riceBlackbeans": "https://www.allrecipes.com/recipe/15559/black-beans-and-rice/",
+      "chapatiBlackbeans": "https://cookpad.com/ke/recipes/2605885-chapati-and-blackbeans",
+      "riceLentilsYogurt": "https://cookieandkate.com/mujaddara-recipe/",
+      "riceBlackBeansChicken": "https://www.tasteofhome.com/recipes/black-bean-chicken-with-rice/",
+      "riceBeans": "https://www.foodnetwork.com/recipes/robert-irvine/red-beans-and-rice-recipe-1950779",
+      "riceBeansCoconut": "https://catholicfoodie.com/ugali-with-bean-soup-from-tanzania-4lent4life",
+      "ugaliBeansVeg": "https://cookpad.com/ke/recipes/3521094-ugali-served-with-beans-stew-and-vegetables",
+      "ugaliFishSukuma": "https://cookpad.com/ke/recipes/3416696-fried-fish-kales-and-kachumbari-with-ugali",
+      "jollofRiceFish": "https://www.sbs.com.au/food/recipes/jollof-rice-fish"
+    }
+  }
+});
