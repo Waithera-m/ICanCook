ecb22ed4e3dc3c50b2d7255c0d6a69439f5fbd69
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-04 17:02:40 +0300

    objects in js

---
+++ js/recipes.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var meals={
+        flour: {
+            "wheatFlour": "https://www.allrecipes.com/recipe/233531/quick-whole-wheat-chapati/",
+            "maizeFlour": "https://recipes.timesofindia.com/recipes/maize-flour-dhoklas/rs59756029.cms",
+            "rice": "https://www.delish.com/cooking/a20089653/how-to-cook-rice/",
+            "pasta": "https://thewanderlustkitchen.com/pasta-for-one-10-minutes/",
+            "beans": "https://www.bbcgoodfood.com/recipes/kidney-bean-curry",
+            "lentils": "https://www.culinaryhill.com/how-to-cook-lentils/",
+            "blackbeans": "https://www.allrecipes.com/recipe/63113/best-black-beans/",
+
+        }
+    }
+})
