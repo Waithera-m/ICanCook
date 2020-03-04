9ce6a67694744d66805ac5b17a36e62cc84e1493
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-04 10:25:06 +0300

    edits in html

--- js/home.js
+++ js/home.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
-    $("#carouselHome").carousel();
+// $(document).ready(function () {
+//     $("#carouselHome").carousel();
 
-    $(".corousel-control-prev").click(function () {
-        $("#carouselHome").carousel("prev")
-    });
-    $(".corousel-control-next").click(function () {
-        $("#carouselHome").carousel("next")
-    });
-    $(".imageOne").click(function () {
-        $("#carouselHome").carousel(0);
-    });
-    $(".imageTwo").click(function () {
-        $("#carouselHome").carousel(1);
-    });
-    $(".imageThree").click(function () {
-        $("#carouselHome").carousel(2);
-    });
-    $(".imageFour").click(function () {
-        $("#carouselHome").carousel(3);
-    });
-});
+//     $(".corousel-control-prev").click(function () {
+//         $("#carouselHome").carousel("prev")
+//     });
+//     $(".corousel-control-next").click(function () {
+//         $("#carouselHome").carousel("next")
+//     });
+//     $(".imageOne").click(function () {
+//         $("#carouselHome").carousel(0);
+//     });
+//     $(".imageTwo").click(function () {
+//         $("#carouselHome").carousel(1);
+//     });
+//     $(".imageThree").click(function () {
+//         $("#carouselHome").carousel(2);
+//     });
+//     $(".imageFour").click(function () {
+//         $("#carouselHome").carousel(3);
+//     });
+// });
 
 
 
