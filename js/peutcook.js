651f10848c25049d186b36bbe1d4ad7d8bf8ab47
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-04 16:09:55 +0300

    activate carousel indicators, and controls

--- js/peutcook.js
+++ js/peutcook.js
@@ -1,29 +1,23 @@
-$(document).ready(function(){
-  function meals (flour, vegetable, meat, dairy){
-    this.flours = {
-      "maizeFlour": "https://cookpad.com/ke/recipes/4727293-how-to-cook-the-perfect-ugali",
-      "wheatflour": "https://www.bigoven.com/recipe/kenyan-soft-layered-chapati/2296480",
-      "legumes": "https://www.soscuisine.com/recipe/mixed-legume-salad"
-    }
-    this.vegetables = {
-      "cabbage": "https://www.wellplated.com/sauteed-cabbage/",
-      "spinach": "https://cafedelites.com/easy-creamed-spinach/"
-    }
-  }
-
-  var meals ={
-    flour:{
-      "maizeFlour": "https://cookpad.com/ke/recipes/4727293-how-to-cook-the-perfect-ugali",
-      "wheatflour": "https://www.bigoven.com/recipe/kenyan-soft-layered-chapati/2296480",
-      "legumes": "https://www.soscuisine.com/recipe/mixed-legume-salad"
-    },
-    vegetables:{
-      "cabbage": "https://www.wellplated.com/sauteed-cabbage/",
-      "spinach": "https://cafedelites.com/easy-creamed-spinach/"
-    },
-
-    combinedmeals:{
-    "ricespinach":"https://www.allrecipes.com/recipe/72500/spinach-rice/"}
-
-  }
-})
+$(document).ready(function () {
+      $("#carouselHome").carousel();
+  
+      $(".corousel-control-prev").click(function () {
+          $("#carouselHome").carousel("prev")
+      });
+      $(".corousel-control-next").click(function () {
+          $("#carouselHome").carousel("next")
+      });
+      $(".imageOne").click(function () {
+          $("#carouselHome").carousel(0);
+      });
+      $(".imageTwo").click(function () {
+          $("#carouselHome").carousel(1);
+      });
+      $(".imageThree").click(function () {
+          $("#carouselHome").carousel(2);
+      });
+      $(".imageFour").click(function () {
+          $("#carouselHome").carousel(3);
+      });
+  });
+  
