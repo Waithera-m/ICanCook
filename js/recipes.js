a2d59bdcf43a3fd0649a94fd40523c01db90da01
Gr3atJes <mathiujes@gmail.com>
2020-03-05 10:56:47 +0300

    changed cases of vegetable items from upper cases to lower cases

--- js/recipes.js
+++ js/recipes.js
@@ -1,14 +1,14 @@
 meals={
     vegetables:{
-        "Tomato": "https://www.bbcgoodfood.com/recipes/collection/tomato/",
-        "Cabbage": "https://www.wellplated.com/sauteed-cabbage/",
-        "Brocolli": " https://www.allrecipes.com/recipe/228221/easy-lemon-and-garlic-broccoli/",
-        "Cauliflower": "https://www.bbcgoodfood.com/recipes/collection/cauliflower",
-        "Spinach": "https://www.simplyrecipes.com/recipes/spinach/",
-        "Sukuma": "https://cookpad.com/ke/recipes/2401121-fried-sukuma-wiki-kales",
-        "Onion": "https://www.simplyrecipes.com/recipes/how_to_caramelize_onions/",
-        "Garlic": "https://cafedelites.com/garlic-mushrooms/",
-        "Tangawizi": " https://tarasmulticulturaltable.com/chai-ya-tangawizi-kenyan-ginger-tea/"
+        "tomato": "https://www.bbcgoodfood.com/recipes/collection/tomato/",
+        "cabbage": "https://www.wellplated.com/sauteed-cabbage/",
+        "brocolli": " https://www.allrecipes.com/recipe/228221/easy-lemon-and-garlic-broccoli/",
+        "cauliflower": "https://www.bbcgoodfood.com/recipes/collection/cauliflower",
+        "spinach": "https://www.simplyrecipes.com/recipes/spinach/",
+        "sukuma": "https://cookpad.com/ke/recipes/2401121-fried-sukuma-wiki-kales",
+        "onion": "https://www.simplyrecipes.com/recipes/how_to_caramelize_onions/",
+        "garlic": "https://cafedelites.com/garlic-mushrooms/",
+        "tangawizi": " https://tarasmulticulturaltable.com/chai-ya-tangawizi-kenyan-ginger-tea/"
     }
 }
 
