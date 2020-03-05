6c0a2c571412a9da58d78169f27f8cc5370a729e
Mary Waithera <njihiamary11@gmail.com>
2020-03-05 14:16:46 +0300

    Remove redundant variable declarations

--- js/recipes.js
+++ js/recipes.js
@@ -25,33 +25,7 @@ $(document).ready(function(){
     }
   };
 function checkOut (){
-  var allPurposeFlour = document.getElementById("allPurposeWheatFlour").value;
-  var maizeFlour = document.getElementById("mflour").value;
-  var rice = document.getElementById("rice").value;
-  var pasta = document.getElementById("pasta").value;
-  var beans = document.getElementById("beans").value;
-  var lentils = document.getElementById("lentils").value;
-  var blackBeans = document.getElementById("blackbeans").value;
-  var milk = document.getElementById("milk").value;
-  var eggs = document.getElementById("eggs").value;
-  var cheddar = document.getElementById("cheddar").value;
-  var parmesan = document.getElementById("parmesan").value;
-  var yogurt = document.getElementById("yogurt").value;
-  var coconutMilk = document.getElementById("coconutMilk").value;
-  var beef = document.getElementById("beef").value;
-  var chicken = document.getElementById("chicken").value;
-  var bacon = document.getElementById("bacon").value;
-  var fish = document.getElementById("fish").value;
-  var pork = document.getElementById("pork").value;
-  var tomatoes = document.getElementById("tomatoes").value;
-  var cabbage = document.getElementById("cabbage").value;
-  var brocolli = document.getElementById("brocolli").value;
-  var cauliflower = document.getElementById("cauliflower").value;
-  var spinach = document.getElementById("spinach").value;
-  var sukuma = document.getElementById("sukuma").value;
-  var onion = document.getElementById("onion").value;
-  var garlic = document.getElementById("garlic").value;
-  var tangawizi = document.getElementById("tangawizi").value;
+ 
 
  //Determine links to return if users select more than one ingredient
  
